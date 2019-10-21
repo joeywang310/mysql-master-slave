20a1b5f31ccc6c0eab303a33f4b9b2bd5c2e2be7
joeywang310 <wangzhicheng310@126.com>
2019-10-21 20:45:19 +0800

    init project

---
+++ src/main/java/com/wzc/mysqlmasterslave/MysqlMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.wzc.mysqlmasterslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlMasterSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlMasterSlaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzc/mysqlmasterslave/MysqlMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzc.mysqlmasterslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlMasterSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
